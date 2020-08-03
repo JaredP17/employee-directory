61b4eee63188ec7f5d4f08d34c2824928c68961b
Jared Porter <jaredp17@me.com>
2020-08-03 19:25:51 -0400

    Hard coded header for page.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <h1>Hello World!</h1>
+    <header>
+      <h1 className="text-center">Employee Directory</h1>
+      <p className="text-center">Click on carrots to filter by heading or use the search box to narrow your results.</p>
+    </header>
     </div>
   );
 };
