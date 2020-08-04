c3f4b3c3075c470f3a78bde90a6ca17517f2b5fc
Jared Porter <jaredp17@me.com>
2020-08-04 00:00:09 -0400

    Hard coded sample table.

--- src/App.js
+++ src/App.js
@@ -8,6 +8,48 @@ const App = () => {
     <>
       <Header />
       <SearchBar />
+      <div className="container">
+        <table class="table table-striped text-center mt-5">
+          <thead>
+            <tr>
+              <th scope="col">Image</th>
+              <th scope="col">Name</th>
+              <th scope="col">Phone</th>
+              <th scope="col">Email</th>
+              <th scope="col">DOB</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">
+                <img src="https://www.fillmurray.com/50/50" alt=""/>
+              </th>
+              <td>Fill Murray</td>
+              <td>(123)-456-7890</td>
+              <td>fill.murray@example.com</td>
+              <td>09-21-1950</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <img src="https://www.fillmurray.com/50/50" alt=""/>
+              </th>
+              <td>Fill Murray</td>
+              <td>(123)-456-7890</td>
+              <td>fill.murray@example.com</td>
+              <td>09-21-1950</td>
+            </tr>
+            <tr>
+              <th scope="row">
+                <img src="https://www.fillmurray.com/50/50" alt=""/>
+              </th>
+              <td>Fill Murray</td>
+              <td>(123)-456-7890</td>
+              <td>fill.murray@example.com</td>
+              <td>09-21-1950</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
