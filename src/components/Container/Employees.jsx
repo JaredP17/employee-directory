29889338f63462614516f5da5e2a8eff2b020212
Jared Porter <jaredp17@me.com>
2020-08-04 09:19:07 -0400

    Search filter works.

--- src/components/Container/Employees.jsx
+++ src/components/Container/Employees.jsx
@@ -7,40 +7,92 @@ class Employees extends Component {
     search: "",
     employees: [],
     filteredEmployees: [],
+    sortAscending: true,
   };
 
   // When this component mounts, load random users as employees from https://randomuser.me/
   componentDidMount() {
     API.getEmployees()
-      .then((res) => this.setState({ employees: res.data.results }))
+      .then((res) =>
+        this.setState({
+          employees: res.data.results,
+          filteredEmployees: res.data.results,
+        })
+      )
       .catch((err) => console.log(err));
   }
+
+  // Update search state to filter by employee name
+  handleInputChange = (event) => {
+    const value = event.target.value;
+    this.setState({ search: value });
+    this.filterEmployees(value.toLowerCase().trim());
+  };
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  sortEmployees = (field, property) => {};
+
+  filterEmployees = (input) => {
+    if (input) {
+      this.setState({
+        filteredEmployees: this.state.employees.filter((employee) => {
+          return employee.name.first.toLowerCase().includes(input) || employee.name.last.toLowerCase().includes(input);
+        }),
+      });
+    } else {
+      this.setState({ filteredEmployees: this.state.employees });
+    }
+  };
 
   render() {
     return (
       <>
-        <SearchBar />
+        <SearchBar
+          value={this.state.search}
+          handleInputChange={this.handleInputChange}
+          handleFormSubmit={this.handleFormSubmit}
+        />
         <div className="container">
           <table className="table table-striped text-center mt-5">
             <thead>
               <tr>
                 <th scope="col">Image</th>
-                <th scope="col">Name</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Email</th>
-                <th scope="col">DOB</th>
+                <th scope="col">
+                  <span onClick={() => console.log("Sort by name")}>Name</span>
+                </th>
+                <th scope="col">
+                  <span onClick={() => console.log("Sort by phone number")}>
+                    Phone
+                  </span>
+                </th>
+                <th scope="col">
+                  <span onClick={() => console.log("Sort by email")}>
+                    Email
+                  </span>
+                </th>
+                <th scope="col">
+                  <span onClick={() => console.log("Sort by dob")}>DOB</span>
+                </th>
               </tr>
             </thead>
             <tbody>
-              {this.state.employees.map((employee) => {
-                  const {first, last} = employee.name;
-                  const fullName = `${first} ${last}`;
-                  const date = new Date(employee.dob.date);
-                  let dob = [];
-                  dob.push(("0" + (date.getMonth() + 1)).slice(-2));
-                  dob.push(("0" + date.getDate()).slice(-2));
-                  dob.push(date.getFullYear());
-                  dob = dob.join("-");
+              {this.state.filteredEmployees.map((employee) => {
+                const { first, last } = employee.name;
+                const fullName = `${first} ${last}`;
+
+                // Format date
+                const date = new Date(employee.dob.date);
+                let dob = [];
+                dob.push(("0" + (date.getMonth() + 1)).slice(-2));
+                dob.push(("0" + date.getDate()).slice(-2));
+                dob.push(date.getFullYear());
+
+                // Join formatted date
+                dob = dob.join("-");
+
                 return (
                   <tr key={employee.login.uuid}>
                     <th scope="row">
@@ -48,7 +100,9 @@ class Employees extends Component {
                     </th>
                     <td>{fullName}</td>
                     <td>{employee.cell}</td>
-                    <td>{employee.email}</td>
+                    <td>
+                      <a href={`mailto:${employee.email}`}>{employee.email}</a>
+                    </td>
                     <td>{dob}</td>
                   </tr>
                 );
