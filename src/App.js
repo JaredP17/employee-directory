519f977b651d3ed6f108585c6b567d211c422032
Jared Porter <jaredp17@me.com>
2020-08-03 20:26:29 -0400

    Styling.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>
-    <header>
-      <h1 className="text-center">Employee Directory</h1>
-      <p className="text-center">Click on carrots to filter by heading or use the search box to narrow your results.</p>
-    </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 };
 
