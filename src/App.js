412b86c2ccb6d9015f21f186bf2cf5f6b657999a
Jared Porter <jaredp17@me.com>
2020-08-04 03:02:56 -0400

    Refactored components.

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
-import SearchBar from "./components/SearchBar";
+import Employees from "./components/Container/Employees"
 
 const App = () => {
   return (
     <>
       <Header />
-      <SearchBar />
-      <div className="container">
-        <table class="table table-striped text-center mt-5">
-          <thead>
-            <tr>
-              <th scope="col">Image</th>
-              <th scope="col">Name</th>
-              <th scope="col">Phone</th>
-              <th scope="col">Email</th>
-              <th scope="col">DOB</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">
-                <img src="https://www.fillmurray.com/50/50" alt=""/>
-              </th>
-              <td>Fill Murray</td>
-              <td>(123)-456-7890</td>
-              <td>fill.murray@example.com</td>
-              <td>09-21-1950</td>
-            </tr>
-            <tr>
-              <th scope="row">
-                <img src="https://www.fillmurray.com/50/50" alt=""/>
-              </th>
-              <td>Fill Murray</td>
-              <td>(123)-456-7890</td>
-              <td>fill.murray@example.com</td>
-              <td>09-21-1950</td>
-            </tr>
-            <tr>
-              <th scope="row">
-                <img src="https://www.fillmurray.com/50/50" alt=""/>
-              </th>
-              <td>Fill Murray</td>
-              <td>(123)-456-7890</td>
-              <td>fill.murray@example.com</td>
-              <td>09-21-1950</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <Employees/>
     </>
   );
 };
--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,27 +1,24 @@
-import React, { Component } from "react";
-import "./style.css"
+import React from "react";
+import "./style.css";
 
-class SearchBar extends Component {
+const SearchBar = (props) => {
   // Setting the component's initial state
-  state = {
-    search: "",
-  };
 
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light justify-content-center">
-        <form className="form-inline m-2">
-          <input
-            className="form-control"
-            name="search"
-            type="search"
-            placeholder="Search"
-            id="search"
-          />
-        </form>
-      </nav>
-    );
-  }
-}
+  return (
+    <nav className="navbar navbar-light bg-light justify-content-center">
+      <form className="form-inline m-2">
+        <input
+          onChange={props.handleInputChange}
+          value={props.search}
+          className="form-control"
+          name="search"
+          type="search"
+          placeholder="Search"
+          id="search"
+        />
+      </form>
+    </nav>
+  );
+};
 
 export default SearchBar;
